a1a2288cc9a946598b092f6c006ff2fdbf320f09
Justin Wayne Wilson <wilsonjustinwayne@gmail.com>
2018-08-07 16:00:50 -0500

    Options 1-3 completed

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import java.io.*;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Option1 option1 = new Option1();
         Option2 option2 = new Option2();
+        Option3 option3 = new Option3();
+
         Scanner sc = new Scanner(System.in);
         System.out.println("1. View contacts.\n" +
                 "2. Add a new contact.\n" +
@@ -14,16 +19,17 @@ public class Main {
                 "5. Exit.\n" +
                 "Enter an option (1, 2, 3, 4 or 5):");
         int userOption = sc.nextInt();
-        String directory = "src";
-        String filename = "contacts.txt";
-        Path dataFile = Paths.get(directory, filename);
+        Path dataFile = Paths.get("src", "contacts.txt");
         List<Contact> contacts = new ArrayList<>();
-        List<String> contents = Files.readAllLines(
-                dataFile);
+        List<String> contents = null;
+        try {
+            contents = Files.readAllLines(
+                    dataFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         for (int i = 0; i < contents.size(); i++) {
-            String name = contents.get(i).substring(0, contents.get(i).indexOf("-"));
-            String number = contents.get(i).substring( contents.get(i).indexOf("-") + 1);
-            contacts.add(new Contact(name, number));
+            contacts.add(new Contact(contents.get(i).substring(0, contents.get(i).indexOf("-")), contents.get(i).substring( contents.get(i).indexOf("-") + 1)));
         }
         switch (userOption) {
             case 1:
@@ -34,10 +40,24 @@ public class Main {
                 option1.printList(contacts);
                 break;
             case 3:
+                Contact contact = option3.searchThroughList(contacts);
+                System.out.println(contact.getName() + " | " + contact.getNumber());
+
+                break;
             case 4:
             default:
 
         }
+        List<String> list = new ArrayList<>();
+        for (Contact contact : contacts) {
+            list.add(contact.getNumber() + "-" + contact.getName());
+        }
+        try {
+            Files.write(dataFile, list);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
 
     }
 }
---
+++ src/Option3.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.*;
+
+class Option3 {
+    Contact searchThroughList(List<Contact> list) {
+        Scanner sc = new Scanner(System.in);
+        Contact contact = new Contact("Contact", "does not exist");
+        System.out.println("Enter name: ");
+        String name = sc.nextLine();
+        for (Contact item : list) {
+            if (item.getName().equals(name)) {
+                contact = item;
+            }
+        }
+        return contact;
+    }
+}
