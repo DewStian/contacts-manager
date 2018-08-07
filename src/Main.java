7e56adafaad80737b075b9212e6cb2ea4a1b1e6b
Christian Kenney <armyystrongg@Christians-MacBook-Air.local>
2018-08-07 14:30:56 -0500

    reading and printing contacts file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.*;
+import java.nio.file.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. View contacts.\n" +
+                "2. Add a new contact.\n" +
+                "3. Search a contact by name.\n" +
+                "4. Delete an existing contact.\n" +
+                "5. Exit.\n" +
+                "Enter an option (1, 2, 3, 4 or 5):");
+        int userOption = sc.nextInt();
+        String directory = "src";
+        String filename = "contacts.txt";
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+        try {
+            List<String> contents = Files.readAllLines(
+                    dataFile);
+            System.out.println(contents);
+        } catch (Exception e) {
+            System.out.println("Exception!");
+            e.printStackTrace();
+        }
+        switch(userOption){
+            case 1 :
+            case 2 :
+            case 3 :
+            case 4 :
+            default :
+
+        }
+
     }
 }
---
+++ src/Option1.java
@@ -0,0 +1,8 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.File;
+public class Option1 {
+
+
+
+}
